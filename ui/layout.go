204c28ebc10d87ab35c4edae127bc8920a49775f
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-08-11 00:50:23 +0530

    splash screen added for GoDo

--- ui/layout.go
+++ ui/layout.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"path/filepath"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"todo-cli/types"
@@ -14,11 +15,64 @@ var pages *tview.Pages
 var currentFile string
 var config types.Config
 
+func splashScreen(dir string) *tview.Flex {
+	asciiArt := 
+`
+     ,o888888o.        ,o888888o.     8 888888888o.          ,o888888o.     
+    8888     '88.   . 8888     '88.   8 8888    '^888.    . 8888     '88.   
+ ,8 8888       '8. ,8 8888       '8b  8 8888        '88. ,8 8888       '8b  
+ 88 8888           88 8888        '8b 8 8888         '88 88 8888        '8b 
+ 88 8888           88 8888         88 8 8888          88 88 8888         88 
+ 88 8888           88 8888         88 8 8888          88 88 8888         88 
+ 88 8888   8888888 88 8888        ,8P 8 8888         ,88 88 8888        ,8P 
+ '8 8888       .8' '8 8888       ,8P  8 8888        ,88' '8 8888       ,8P  
+    8888     ,88'   ' 8888     ,88'   8 8888    ,o88P'    ' 8888     ,88'   
+     '8888888P'        '8888888P'     8 888888888P'          '8888888P'     
+`
+
+	splashText := tview.NewTextView().
+		SetText(asciiArt).
+		SetTextAlign(tview.AlignCenter)
+
+	message := tview.NewTextView().
+		SetText("Press Enter to continue").
+		SetTextAlign(tview.AlignCenter)
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(nil, 0, 1, false).
+		AddItem(splashText, 0, 1, false).
+		AddItem(message, 1, 1, false).
+		AddItem(nil, 0, 1, false)
+
+	// Only react to Enter key
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEnter {
+			initializeMainUI(dir)
+			pages.RemovePage("splash") // Clean up splash page
+			return nil                 // Consume the Enter key
+		}
+		return nil // Ignore all other keys
+	})
+
+	return flex
+}
+
 func StartApp(dir string, cfg types.Config) {
 	config = cfg
 	app = tview.NewApplication()
 	pages = tview.NewPages()
 
+	// Show splash
+	pages.AddPage("splash", splashScreen(dir), true, true)
+	app.SetRoot(pages, true)
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func initializeMainUI(dir string) {
 	fileListView = tview.NewList()
 	updateFileListTitle()
 
@@ -37,15 +91,11 @@ func StartApp(dir string, cfg types.Config) {
 		AddItem(fileListView, 30, 1, true).
 		AddItem(todoListView, 0, 2, false)
 
-	pages.AddPage("main", flex, true, true)
+	pages.AddPage("main", flex, true, false)
+	pages.SwitchToPage("main")
 	setFocus(fileListView)
-	app.SetRoot(pages, true)
 
 	registerKeybindings(dir)
-
-	if err := app.Run(); err != nil {
-		panic(err)
-	}
 }
 
 func updateFileListTitle() {
